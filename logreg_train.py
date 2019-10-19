9e9449d18963554cba253aa33bdf67de48dc8cc3
Amina MANSOUR <amansour@e1r11p20.42.fr>
2019-10-19 16:28:46 +0200

    modify logreg

--- logreg_train.py
+++ logreg_train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import sys
+import matplotlib.pyplot as plt
 
 def logistic_func(theta, X): 
     "logistic(sigmoid) function"
@@ -17,7 +18,6 @@ def cost_func(theta, X, y):
     "cost function, J "
 
     log_func_v = logistic_func(theta, X)
-    #print(v.dot(np.log(1 - log_func_v)))
     step1 = y * np.log(log_func_v)
     step2 = (1 - y) * np.log(1 - log_func_v)
     if np.isnan(step2):
@@ -25,20 +25,21 @@ def cost_func(theta, X, y):
     final = -step1 - step2
     return np.mean(final)
 
-def grad_desc(df, value, lr=.01, converge_change=.001):
+def grad_desc(X, value, lr=.01, converge_change=1):
     "gradient descent function"
-    X = df.values
-    theta = np.matrix(np.zeros(X.shape[1]))
+    theta = np.matrix(np.zeros(X.shape[1])) 
     cost = cost_func(theta, X, value)
     change_cost = 1
     num_iter = 1
-    while (change_cost > converge_change):
-        print(cost) 
+    costs = []
+    costs.append(cost)
+    for i in range(10000):
         old_cost = cost
         theta = theta - (lr * log_gradient(theta, X, value))
         cost = cost_func(theta, X, value)
         change_cost = old_cost - cost 
         num_iter += 1
+        costs.append(cost)
     return theta, num_iter
 
 def pred_values(theta, X):
@@ -52,8 +53,9 @@ def scale_data(df):
 
 def logistic_regression(houseName, df):
     value = []
-    for x in df['Hogwarts House']:
-        if x == houseName:
+    print(df)
+    for house in df['Hogwarts House']:
+        if house == houseName:
             value.append(1)
         else:
             value.append(0)
@@ -61,9 +63,9 @@ def logistic_regression(houseName, df):
     del df['Hogwarts House']
     "scale data"
     df = scale_data(df)
-    theta, num_iter = grad_desc(df, value)
-    print(num_iter)
-    #print(theta, num_iter)
+    "important ;)"
+    X = np.hstack((np.matrix(np.ones(df.shape[0])).T, df))
+    theta, num_iter = grad_desc(X, value)
     return theta
 
 
@@ -83,6 +85,9 @@ if __name__ == '__main__':
         "Data of Ravenclaw house"
         df = df.dropna()
         theta_dic['Ravenclaw'] = logistic_regression('Ravenclaw', df)
+        theta_dic['Slytherin'] = logistic_regression('Slytherin', df)
+        theta_dic['Gryffindor'] = logistic_regression('Gryffindor', df)
+        theta_dic['Hufflepuff'] = logistic_regression('Hufflepuff', df)
     except:
         print("Usage: python3 logreg_train.py resources/dataset_train.csv")
         exit (-1)
